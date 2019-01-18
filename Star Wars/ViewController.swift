572ad65d4dd1164147034d93a748f48416b14c52
Saqo <saqo@saqos-macbook-air.local>
2019-01-18 21:30:16 +0400

    Initial Commit

---
+++ Star Wars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Wars
+//
+//  Created by Saqo on 1/18/19.
+//  Copyright © 2019 Arcsinus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
